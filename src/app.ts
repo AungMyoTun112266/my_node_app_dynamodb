198e09288acd078cc3a7f80f7a708f37f8769e8a
AungMyoTun112266 <aungmyotun1115@gmail.com>
2024-09-10 16:11:54 +0700

    Add Some Security Mechanician

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,37 @@ import bodyParser from 'body-parser';
 import userRoutes from './routes/userRoutes';
 import { loggingMiddleware } from './middlewares/loggingMiddleware';
 import errorHandler from './middlewares/errorHandler';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 // import { authMiddleware } from './middlewares/authMiddleware';
 
 const app = express();
+
+
+// Customize Helmet configuration
+app.use(helmet({
+    contentSecurityPolicy: {
+      useDefaults: true,
+      directives: {
+        ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+        'img-src': ["'self'", 'data:'],
+        'script-src': ["'self'"],
+      },
+    },
+    crossOriginOpenerPolicy: { policy: 'same-origin' },
+    crossOriginResourcePolicy: { policy: 'same-origin' },
+    referrerPolicy: { policy: 'no-referrer' },
+    strictTransportSecurity: { maxAge: 31536000, includeSubDomains: true },
+    xssFilter: true,
+  }));
+
+
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: 'Too many requests from this IP, please try again later.'
+  });
+app.use(limiter);
 
 app.use(bodyParser.json());
 
